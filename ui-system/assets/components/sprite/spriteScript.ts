7df5fbf39deb28dc22c00608cd8a6802e897cb1e
李威 <liweijsj@163.com>
2025-04-08 11:24:53 +0800

    dynamic load image resource

--- ui-system/assets/components/sprite/spriteScript.ts
+++ ui-system/assets/components/sprite/spriteScript.ts
@@ -1,14 +1,17 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, resources, Sprite, SpriteFrame } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('sprite')
 export class sprite extends Component {
     start() {
-
+        resources.load("ai/spriteFrame", SpriteFrame, (err, spriteFrame) => {
+            this.node.getComponent(Sprite).spriteFrame = spriteFrame;
+        });
     }
 
     update(deltaTime: number) {
-        
+
     }
 }
-
+
+
