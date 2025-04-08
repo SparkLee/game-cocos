f73f0dae5a0a5a007f8e842ff5bee387840f2dc5
李威 <liweijsj@163.com>
2025-04-08 16:16:32 +0800

    使用resources.load()加载图像资源

--- ui-system/assets/components/sprite/spriteScript.ts
+++ ui-system/assets/components/sprite/spriteScript.ts
@@ -1,11 +1,25 @@
-import { _decorator, Component, Node, resources, Sprite, SpriteFrame } from 'cc';
+import { _decorator, Component, ImageAsset, log, Node, resources, Sprite, SpriteFrame, Texture2D } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('sprite')
 export class sprite extends Component {
     start() {
-        resources.load("ai/spriteFrame", SpriteFrame, (err, spriteFrame) => {
-            this.node.getComponent(Sprite).spriteFrame = spriteFrame;
+        // resources.load("ai/spriteFrame", SpriteFrame, (err, spriteFrame) => {
+        //     this.node.getComponent(Sprite).spriteFrame = spriteFrame;
+        // });
+
+        resources.load("ai", ImageAsset, (err, imageAsset) => {
+            const tex = new Texture2D();
+            tex.image = imageAsset;
+
+            const sf = new SpriteFrame();
+            sf.texture = tex;
+
+            const sprite = this.node.getComponent(Sprite);
+            sprite.spriteFrame = sf;
+
+            // log(imageAsset);
+            // log(imageAsset.nativeUrl)
         });
     }
 
